9fd02b989aa39f1520a69dcea971c6dbf1e7221c
Matthew Fudge <Matthew Fudge@10.0.0.253>
2020-06-14 11:21:39 -0600

    Created program starting point

---
+++ src/ArtiiApp.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class ArtiiApp {
+
+	public static final String ROOT_DIR = System.getProperty("user.dir");
+	
+	public ArtiiApp() {
+		
+	}
+	
+	public void run() {
+		
+	}
+	
+	
+}
