cfb14748a78ceba4beaf60bb73dcad56e9c95d4a
Matthew Fudge <Matthew Fudge@10.0.0.253>
2020-06-23 10:28:06 -0600

    Added class documentation

--- src/ArtiiApp.java
+++ src/ArtiiApp.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-
+/**
+ * AsciiArtii - ArtiiApp class
+ * 
+ * The ArtiiApp class handles main duties of the program such as displaying
+ * the menu, reading user choice, and loading a new AsciiArt file. It primarily
+ * handles user interaction with the program.
+ * 
+ * @author Matthew Fudge
+ *
+ */
 public class ArtiiApp {
 
 	public static final String ROOT_DIR = System.getProperty("user.dir");
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,13 @@
+/**
+ * AsciiArtii
+ * 
+ * This program creates .png images out of AsciiArt text files.
+ * Some simple information about the art is also displayed, such
+ * as the characters used and the size of the artwork.
+ * 
+ * @author Matthew Fudge
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
