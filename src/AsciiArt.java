b0dd463045e03a0d31d0172b73fb99e66bce4a65
Matthew Fudge <Matthew Fudge@10.0.0.253>
2020-06-23 16:19:53 -0600

    Implemented initialization + getRows()

--- src/AsciiArt.java
+++ src/AsciiArt.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AsciiArt {
 
@@ -23,6 +24,8 @@ public class AsciiArt {
 	private ArrayList<String> rows;
 	private ArrayList<Character> uniqueChars;
 	
+	private Scanner artScanner;
+	
 	/**
 	 * Initializes all instance variables.
 	 * 
@@ -36,7 +39,53 @@ public class AsciiArt {
 		numChars = 0;
 		rows = new ArrayList<String>();
 		uniqueChars = new ArrayList<Character>();
+		artScanner = new Scanner(DEFAULT_ART);
 		
+		initialize();
+	}
+	
+	/**
+	 * Retrieves necessary data for the AsciiArt being used. This includes the
+	 * art's height, width, number of characters used, and a list of each unique
+	 * character in the art.
+	 */
+	private void initialize() {
+		rows = getRows();
+		imageHeight = getHeight();
+		imageWidth = getWidth();
+	}
+	
+	/**
+	 * Returns the number of lines ("rows") that the ASCII art uses. An ArrayList
+	 * is used to store each row; each String element in the ArrayList is a single row
+	 * of the ASCII art.
+	 * 
+	 * @return rows of art
+	 */
+	private ArrayList<String> getRows() {
+		ArrayList<String> rowsOfArt = new ArrayList<String>();
+		while (artScanner.hasNextLine()) {
+			rowsOfArt.add(artScanner.nextLine());
+		}
+		return rowsOfArt;
+	}
+	
+	/**
+	 * Returns the height of the ASCII art.
+	 * 
+	 * @return height of art
+	 */
+	private int getHeight() {
+		return 0;
+	}
+	
+	/**
+	 * Returns the width of the ASCII art.
+	 * 
+	 * @return width of art
+	 */
+	private int getWidth() {
+		return 0;
 	}
 	
 }
