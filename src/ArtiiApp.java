c3a9d38cad1387ee64a0b0bf83eb86a8fac43869
Matthew Fudge <Matthew Fudge@10.0.0.253>
2020-06-23 10:39:28 -0600

    Implemented menu displaying

--- src/ArtiiApp.java
+++ src/ArtiiApp.java
@@ -14,11 +14,40 @@ public class ArtiiApp {
 
 	public static final String ROOT_DIR = System.getProperty("user.dir");
 	
+	private Scanner keyboard;
+	
 	public ArtiiApp() { 
-		
+		keyboard = new Scanner(System.in);
 	}
 	
+	/**
+	 * Displays a welcome message and then displays the current ASCII art
+	 * info + the main menu. Also waits for user choices until user decides
+	 * to quit.
+	 */
 	public void run() {
-		
+		String choice = "0";
+		displayWelcome();
+		while (!choice.equals("4")) {
+			displayDivider();
+			displayMenu();
+			choice = keyboard.nextLine();
+		}
+	}
+	
+	private void displayWelcome() {
+		System.out.println("Welcome to ArtiiApp!");
+	}
+	
+	private void displayDivider() {
+		System.out.println("***************************************************");
+	}
+	
+	private void displayMenu() {
+		System.out.println("1 - save current ascii art as png");
+		System.out.println("2 - save current ascii art as hex colors");
+		System.out.println("3 - load ascii art file");
+		System.out.println("4 - quit\n");
+		System.out.print("Choice? ");
 	}
 }
