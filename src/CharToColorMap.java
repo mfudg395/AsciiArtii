b42bc7b91eccd5feda9c2f953fa01328018c9b7b
Matthew Fudge <Matthew Fudge@10.0.0.253>
2020-06-29 11:16:26 -0600

    Implemented constructors

--- src/CharToColorMap.java
+++ src/CharToColorMap.java
@@ -1,3 +1,45 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CharToColorMap {
 
+	private static final String DEFAULT_COLOR = "#FFFFFF";
+
+	private ArrayList<String> mapFileLines;
+	private String hexColor;
+	private Scanner scanner;
+
+	/**
+	 * Constructor. Initializes the ArrayList meant to hold the map file's
+	 * contents. This is only used for the default AsciiArt.
+	 */
+	public CharToColorMap() {
+		mapFileLines = new ArrayList<>();
+	}
+	
+	/**
+	 * Constructor. Fills the ArrayList holding the given file's contents.
+	 *
+	 * @param filePath the absolute path to the map file
+	 */
+	public CharToColorMap(String filePath) {
+		File f = new File(filePath);
+		mapFileLines = new ArrayList<>();
+		scanner = null;
+		hexColor = DEFAULT_COLOR;
+		try {
+			scanner = new Scanner(f);
+			while (scanner.hasNextLine()) {
+				mapFileLines.add(scanner.nextLine());
+			}
+		} catch (FileNotFoundException e) {
+			System.err.println("Error: Cannot load .map file." + e);
+		} finally {
+			if (scanner != null) {
+				scanner.close();
+			}
+		}
+	}
 }
