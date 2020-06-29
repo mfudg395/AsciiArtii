be0a7dd3e6a27030aca1e9a1ac5d92db5f9e4ba1
Matthew Fudge <Matthew Fudge@10.0.0.253>
2020-06-29 11:08:10 -0600

    Implemented constructor

--- src/AsciiConverter.java
+++ src/AsciiConverter.java
@@ -1,4 +1,24 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
+/**
+ * AsciiArtii - AsciiConverter class
+ * 
+ * AsciiConverter handles the conversion of a given ASCII art to either
+ * a .png or a .hex file. 
+ * 
+ * .png files are standard images, while .hex files are a representation of
+ * the image with each pixel replaced by a color hexcode.
+ * 
+ * @author Matthew Fudge
+ */
 public class AsciiConverter {
-
+	
+	private final AsciiArt asciiArt;
+	private final CharToColorMap colorMap;
+	
+	public AsciiConverter(AsciiArt asciiArt, CharToColorMap colorMap) {
+		this.asciiArt = asciiArt;
+		this.colorMap = colorMap;
+	}
 }
