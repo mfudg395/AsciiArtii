968cd3f46bf2bb3944d440eb748349c04bc3959d
Matthew Fudge <Matthew Fudge@10.0.0.253>
2020-07-01 10:44:34 -0600

    Implmenented color setting for each BufferedImage pixel

--- src/AsciiConverter.java
+++ src/AsciiConverter.java
@@ -1,3 +1,4 @@
+import java.awt.image.BufferedImage;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
@@ -14,12 +15,17 @@ import java.io.PrintWriter;
  */
 public class AsciiConverter {
 	
+	private static final int HEXIDECIMAL_BASE = 16;
+	
 	private final AsciiArt asciiArt;
 	private final CharToColorMap colorMap;
+	private final BufferedImage image;
 	
 	public AsciiConverter(AsciiArt asciiArt, CharToColorMap colorMap) {
 		this.asciiArt = asciiArt;
 		this.colorMap = colorMap;
+		this.image = new BufferedImage(asciiArt.getImageWidth(), asciiArt.getImageHeight(), BufferedImage.TYPE_INT_RGB);
+		setColors();
 	}
 	
 	/**
@@ -43,6 +49,21 @@ public class AsciiConverter {
 		} finally {
 			if (writer != null) {
 				writer.close();
+			}
+		}
+	}
+	
+	/**
+	 * Sets each pixel of the BufferedImage to it's appropriate color. The BufferedImage is a
+	 * representation of the ASCII art, so colors are determined based on the AsciiArt's associated
+	 * ColorMap.
+	 */
+	private void setColors() {
+		for (int x = 0; x < image.getWidth(); x++) {
+			for (int y = 0; y < image.getHeight(); y++) {
+				String hexCode = colorMap.colorFor(asciiArt.charAt(x, y));
+				int hexCodeAsInt = Integer.parseInt(hexCode.substring(1), HEXIDECIMAL_BASE);
+				image.setRGB(x, y, hexCodeAsInt);
 			}
 		}
 	}
